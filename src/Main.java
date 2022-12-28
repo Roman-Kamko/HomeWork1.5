1e5449ab0d1699ff738483f8c644a60d21021d8f
RomanKamko <r.kamko@mail.ru>
2022-12-28 19:35:46 +0400

    выполнил все задания к уроку "Условные операторы. Часть 2"

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,97 @@
 public class Main {
     public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+    }
 
+    public static void task1() {
+        System.out.println("Задача 1");
+        int clientOS = 1;
+
+        if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+        int clientOS = 0;
+        int clientDeviceYear = 2_014;
+        boolean oldClientOS = clientDeviceYear < 2_015;
+
+        if (clientOS == 1) {
+            if (oldClientOS) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            }
+        } else if (clientOS == 0) {
+            if (oldClientOS) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            }
+        }
+    }
+
+    public static void task3() {
+        System.out.println("Задача 3");
+        int year = 2021;
+
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(year + " является високосным");
+        } else {
+            System.out.println(year + " не является високосным");
+        }
+    }
+
+    public static void task4() {
+        System.out.println("Задача 4");
+        int deliveryDistance = 95;
+
+        if (deliveryDistance < 20) {
+            System.out.println("Потребуется дней: 1");
+        } else if (deliveryDistance < 60) {
+            System.out.println("Потребуется дней: 2");
+        } else if (deliveryDistance < 100) {
+            System.out.println("Потребуется дней: 3");
+        } else {
+            System.out.println("Доставки нет");
+        }
+    }
+
+    public static void task5() {
+        System.out.println("Задача 5");
+        int month = 12;
+
+        switch (month) {
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("Зимний сезон");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Весенний сезон");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Летний сезон");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Осенний сезон");
+                break;
+            default:
+                System.out.println("Нет такого месяца");
+        }
     }
 }
